49d67fbb218227fbd23187b2eed791e4086489bf
Bela <belahamid@gmail.com>
2020-11-26 12:01:15 +0000

    divisible by 5 test passed

--- tests/fizzbuzz_tests.py
+++ tests/fizzbuzz_tests.py
@@ -6,8 +6,14 @@ class TestFizzBuzz(unittest.TestCase):
     def test_fizz_buzz__3_returns_fizz(self):
         self.assertEqual("fizz", fizz_buzz(3))
 
+    def test_fizz_buzz_6_returns_fizz(self):
+        self.assertEqual("fizz", fizz_buzz(6))
+
     def test_fizz_buzz_5_returns_buzz(self):
         self.assertEqual("buzz", fizz_buzz(5))
+
+    def test_fizz_buzz_10_returns_fizzbuzz(self):
+        self.assertEqual("buzz", fizz_buzz(10))
 
     def test_fizz_buzz_15_returns_fizzbuzz(self):
         self.assertEqual("fizzbuzz", fizz_buzz(15))
